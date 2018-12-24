c173dc134f205341470ab04d0ccd44a2a14f8216
vavoulas <vavoulasnikos@gmail.com>
2018-12-25 00:25:44 +0200

    service worker commit

--- src/components/Search.js
+++ src/components/Search.js
@@ -9,95 +9,81 @@ class Search extends Component {
       apiUrl: 'https://api.chucknorris.io',
 
       jokes: [],
-      title:''
+      title: ''
     }
-    this.handleChange = this.handleChange.bind(this);
-    this.searchQuery = this.searchQuery.bind(this);
-    
-    
+    this.handleChange = this.handleChange.bind(this)
+    this.searchQuery = this.searchQuery.bind(this)
   }
-  handleChange(e) {
-    this.setState({queryText: e.target.value});
+  handleChange (e) {
+    this.setState({ queryText: e.target.value })
   }
   searchQuery (e) {
-    
-    
     this.setState(
       {
         [e.target.name]: this.state.queryText
       },
       () => {
         if (this.state.queryText === '') {
-          this.setState({ jokes: [], title: "" })
-        }
-        else {
+          this.setState({ jokes: [], title: '' })
+        } else {
           axios
             .get(
               `${this.state.apiUrl}/jokes/search?query=${this.state.queryText}`
             )
-            .then(res => this.setState({ jokes: res.data.result , title: "Jokes"}))
+            .then(res =>
+              this.setState({ jokes: res.data.result, title: 'Jokes' })
+            )
             .catch(err => console.log(err))
-            
         }
       }
     )
-    e.preventDefault();
+    e.preventDefault()
   }
 
- 
   render () {
-    
     return (
       <div className='container'>
-        <div className="row justify-content-center">
-          <div class="col-12 col-md-10 col-lg-8">
-          
-            
-              <form  className="card card-sm" onSubmit={this.searchQuery}>
-                <div className="card-body row no-gutters align-items-center">
-                  <div className="col">
-                    <input
-                      name='queryText'
-                      type='text'
-                      placeholder='Search a Chuck Norris Joke'
-                      className='form-control form-control-lg form-control-borderless '
-                      value={this.state.queryText} 
-                      onChange={this.handleChange} 
-                    />
-                  </div>
-                  <div className="col-auto">
-                    <button 
-                  
-                      type='submit'
-                      value='Search'
-                      className='btn btn-primary'>
-                      
-                      Search
-                    </button>
-                  </div>
+        <div className='row justify-content-center'>
+          <div class='col-12 col-md-10 col-lg-8'>
+            <form className='card card-sm' onSubmit={this.searchQuery}>
+              <div className='card-body row no-gutters align-items-center'>
+                <div className='col'>
+                  <input
+                    name='queryText'
+                    type='text'
+                    placeholder='Search a Chuck Norris Joke'
+                    className='form-control form-control-lg form-control-borderless '
+                    value={this.state.queryText}
+                    onChange={this.handleChange}
+                  />
                 </div>
-                
-
-                
-              </form>
-          </div>  
-          
-        </div> 
-        <br/>
-        <div className="out">
-          <h1 className="text-center">{this.state.title}</h1>
-         
-           
-        
-         
+                <div className='col-auto'>
+                  <button
+                    type='submit'
+                    value='Search'
+                    className='btn btn-primary'>
+                    Search
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+        <br />
+        <div className='out'>
+          <h1 className='text-center'> {this.state.title} </h1>
           <ul className='list-group'>
-          {this.state.jokes.map(bloop => 
-              <li className = "list-group-item d-flex flex-column justify-content-between ">
-                <img className="align-self-center mb-1" src={bloop.icon_url} alt=""/>
-                <p className="text center">{bloop.value}</p>
+            
+            {this.state.jokes.map(bloop => (
+              <li className='list-group-item d-flex flex-column justify-content-between '>
+                <img
+                  className='align-self-center mb-1'
+                  src={bloop.icon_url}
+                  alt=''
+                />
+                <p className='text-center'> {bloop.value} </p>{' '}
               </li>
-            )}
-          
+            ))}
           </ul>
         </div>
       </div>
@@ -105,4 +91,4 @@ class Search extends Component {
   }
 }
 
-export default Search;
+export default Search
